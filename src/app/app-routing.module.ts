29fc7e4a887ac9daf5cfebe462206dd3665e6e3c
jestin-g <jestingabriel@gmail.com>
2021-03-03 16:34:45 +0100

    ajout titre pour tous les produits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { ItemsComponent } from './components/items/items.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: '', component: ItemsComponent },
-  { path: 'ordinateurs', component: ItemsComponent, data: { category: 'pc', categoryLabel: 'Ordinateur' }},
-  { path: 'ordinateurs/portable', component: ItemsComponent, data: { category: 'pc-portable', categoryLabel: 'Ordinateurs Portables' }},
-  { path: 'ordinateurs/bureau', component: ItemsComponent, data: { category: 'pc-bureau', categoryLabel: 'Ordinateurs de Bureau' }},
-  { path: 'ordinateurs/accessoires', component: ItemsComponent, data: { category: 'pc-accessoire', categoryLabel: 'Accessoires d\'ordinateurs' }},
+  { path: '', component: ItemsComponent, data: { categoryLabel: 'Tous les produits' } },
+  { path: 'ordinateurs', component: ItemsComponent, data: { category: 'pc', categoryLabel: 'Ordinateur' } },
+  { path: 'ordinateurs/portable', component: ItemsComponent, data: { category: 'pc-portable', categoryLabel: 'Ordinateurs Portables' } },
+  { path: 'ordinateurs/bureau', component: ItemsComponent, data: { category: 'pc-bureau', categoryLabel: 'Ordinateurs de Bureau' } },
+  { path: 'ordinateurs/accessoires', component: ItemsComponent, data: { category: 'pc-accessoire', categoryLabel: 'Accessoires d\'ordinateurs' } },
 
 ];
 
